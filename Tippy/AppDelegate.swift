b3300721ec2bd04ce00f85b9acb5874db01aca15
Nisha Shah <nisha_shah@intuit.com>
2017-03-13 02:55:02 -0700

    changed ui, added split functionality

--- Tippy/AppDelegate.swift
+++ Tippy/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 79.0/255.0, green: 191.0/255.0, blue: 149.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
         return true
     }
 
--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    
+    @IBOutlet weak var splitCount: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,21 +32,30 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         let default_tip = UserDefaults.standard.value(forKey: "default_tip") as? Int ?? 0
         tipControl.selectedSegmentIndex = default_tip
-        calculateTip(default_tip)
+        calculate()
     }
     
     @IBAction func onTap(_ sender: Any) {
-        print("Hello");
         view.endEditing(true);
     }
     
+    @IBAction func splitChanged(_ sender: Any) {
+        calculate()
+    }
+    
     @IBAction func calculateTip(_ sender: Any) {
+        calculate()
+    }
+    
+    func calculate() {
         let tipPercentages = [0.18, 0.2, 0.25];
-        let bill = Double(billField.text!) ?? 0
+        let split = Double(splitCount.text!) ?? 1
+        var bill = Double(billField.text!) ?? 0
+        bill = bill / split
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex];
         let total = bill + tip
-        tipLabel.text = String(format: "$%.2f",tip)
+        tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f",total)
-    }    
+    }
 }
 
